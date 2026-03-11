e0779dad20a5c1c169d839d860507491fef6aff5
Masud Hasan <2masudhasan2003@gmail.com>
2026-03-11 20:29:20 +0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -3,83 +3,80 @@ import numpy as np
 import pickle
 from tensorflow.keras.models import load_model
 
-# Page config
-st.set_page_config(page_title="AQI Prediction using LSTM", layout="centered")
-
-st.title("🌫️ Air Quality Index Prediction")
-st.write("Predict AQI using LSTM Model")
+st.title("AQI Prediction using LSTM")
 
 # Load model
 model = load_model("lstm_model.h5")
 
 # Load scaler
-with open("scaler.pkl", "rb") as f:
-    scaler = pickle.load(f)
+scaler = pickle.load(open("scaler.pkl","rb"))
 
-st.subheader("Enter Environmental Data")
+st.header("Input Features")
 
-# User inputs
-tavg = st.number_input("Average Temperature")
-tmin = st.number_input("Minimum Temperature")
-tmax = st.number_input("Maximum Temperature")
-prcp = st.number_input("Precipitation")
-wspd = st.number_input("Wind Speed")
-pres = st.number_input("Pressure")
+features = []
+
+tavg = st.number_input("tavg")
+features.append(tavg)
+
+tmin = st.number_input("tmin")
+features.append(tmin)
+
+tmax = st.number_input("tmax")
+features.append(tmax)
+
+prcp = st.number_input("prcp")
+features.append(prcp)
+
+wspd = st.number_input("wspd")
+features.append(wspd)
+
+pres = st.number_input("pres")
+features.append(pres)
 
 Temperature = st.number_input("Temperature")
+features.append(Temperature)
+
 Humidity = st.number_input("Humidity")
-Wind_Speed = st.number_input("Wind Speed (Alt)")
-Pressure = st.number_input("Pressure (Alt)")
+features.append(Humidity)
 
-aqi_lag1 = st.number_input("Previous Day AQI")
+Wind_Speed = st.number_input("Wind_Speed")
+features.append(Wind_Speed)
 
-season = st.selectbox(
-    "Season",
-    [1,2,3,4]
-)
+Pressure = st.number_input("Pressure")
+features.append(Pressure)
+
+aqi_lag1 = st.number_input("AQI_lag1")
+features.append(aqi_lag1)
+
+season = st.number_input("season")
+features.append(season)
 
 PM25 = st.number_input("PM2.5")
+features.append(PM25)
+
 PM10 = st.number_input("PM10")
+features.append(PM10)
+
 O3 = st.number_input("O3")
+features.append(O3)
+
 NO2 = st.number_input("NO2")
+features.append(NO2)
+
 SO2 = st.number_input("SO2")
+features.append(SO2)
+
 CO = st.number_input("CO")
+features.append(CO)
 
-# Feature list
-features = [
-    tavg,tmin,tmax,prcp,wspd,pres,
-    Temperature,Humidity,Wind_Speed,Pressure,
-    aqi_lag1,season,
-    PM25,PM10,O3,NO2,SO2,CO
-]
-
-# Predict button
-if st.button("Predict AQI"):
+if st.button("Predict"):
 
     data = np.array(features).reshape(1,-1)
 
-    # Scale data
     data_scaled = scaler.transform(data)
 
-    # LSTM expects 3D input
     data_scaled = data_scaled.reshape(1,1,data_scaled.shape[1])
 
     prediction = model.predict(data_scaled)
 
-    aqi_value = prediction[0][0]
-
-    st.success(f"Predicted AQI: {aqi_value:.2f}")
-
-    # AQI Category
-    if aqi_value <= 50:
-        st.info("Good Air Quality")
-    elif aqi_value <= 100:
-        st.info("Moderate Air Quality")
-    elif aqi_value <= 150:
-        st.warning("Unhealthy for Sensitive Groups")
-    elif aqi_value <= 200:
-        st.warning("Unhealthy")
-    elif aqi_value <= 300:
-        st.error("Very Unhealthy")
-    else:
-        st.error("Hazardous")
+    st.success(f"Predicted AQI: {prediction[0][0]}")
